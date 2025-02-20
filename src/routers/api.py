d4f97d4299785943f4eef437694bf2b170ec00db
Rishabhpanacea <rishabh@panaceainfosec.com>
2025-02-20 23:43:32 +0530

    Update api.py

--- src/routers/api.py
+++ src/routers/api.py
@@ -22,6 +22,8 @@ from src.configuration.config import DICOM_TEMP_PATH,JPEG_TEMP_PATH
 from fastapi import Response
 from src.utils import CommonUtils
 from src.configuration.config import OutputDir
+from zipfile import ZipFile
+import glob
 
 
 router = APIRouter()
@@ -32,6 +34,97 @@ async def segment_dicom(file: UploadFile = File(...)):
         with tempfile.NamedTemporaryFile(delete=False, suffix=".dcm") as temp_dicom:
             DicomPath = temp_dicom.name
             temp_dicom.write(await file.read())
+
+
+        output_filename = os.path.basename(DicomPath).replace(".dcm", ".jpg")
+        output_path = os.path.join(JPEG_TEMP_PATH, output_filename)
+        
+        pixel_array=dicom_to_array(DicomPath,output_path,format="jpg")
+        mask=mask_output(pixel_array,model)
+        mask = (mask > 0).astype(np.uint8)
+        print(f"Mask shape: {mask.shape}")
+        print(mask.dtype)  # Should be uint8 or int
+
+        print(f"Unique values in mask: {np.unique(mask)}")
+        if mask is None:
+            raise HTTPException(status_code=500, detail="Error generating segmentation mask")
+
+        source_images = []
+                
+        source_images.append(pydicom.dcmread(DicomPath))
+
+        description = hd.seg.SegmentDescription(
+            segment_number=1,
+            segment_label='Fracture',
+            segmented_property_category=codes.SCT.Blood,
+            segmented_property_type=codes.SCT.Blood,
+            algorithm_type=hd.seg.SegmentAlgorithmTypeValues.MANUAL,
+        )
+
+        seg_obj = hd.seg.Segmentation(
+                        source_images=source_images,
+                        pixel_array=mask,
+                        segmentation_type=hd.seg.SegmentationTypeValues.BINARY,
+                        segment_descriptions=[description],
+                        series_instance_uid=hd.UID(),
+                        series_number=1,
+                        sop_instance_uid=hd.UID(),
+                        instance_number=1,
+                        manufacturer='Radpretation ai',
+                        manufacturer_model_name='Brain structure Segmentation Algorithm',
+                        software_versions='0.0.1',
+                        device_serial_number='1234567890'
+                    )
+        seg_obj_path  = os.path.join(OutputDir,'AI_SEG.dcm')
+        seg_obj.save_as(seg_obj_path)
+
+        temp_dir_return = tempfile.mkdtemp(dir=DICOM_TEMP_PATH)
+        shutil.move(seg_obj_path, temp_dir_return)
+        data = {
+            "file_id": os.path.basename(temp_dir_return)
+        }
+
+        return JSONResponse(content=data)
+
+        # return StreamingResponse(
+        #     open(temp_file_path, "rb"),
+        #     media_type="application/dicom",
+        #     headers={"Content-Disposition": f'attachment; filename="segmented.dcm"'},
+        # )
+    except Exception as e:
+        error_trace = traceback.format_exc()  # Get detailed error traceback
+        print("Error Traceback:", error_trace)  # Log it for debugging
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}\n\nTraceback:\n{error_trace}")
+
+
+
+
+@router.post("/segment_dicomv2/")
+async def segment_dicom(file: UploadFile = File(...)):
+    temp_dir = tempfile.mkdtemp(dir=DICOM_TEMP_PATH)
+    try:
+        temp_file = os.path.join(temp_dir, file.filename)
+        with open(temp_file, "wb") as out_file:
+            out_file.write(await file.read())
+
+        with ZipFile(temp_file, 'r') as zip_ref:
+            root_dir = zip_ref.namelist()[0].split('/')[0]  # Find the root dir in the zip
+            zip_ref.extractall(temp_dir)
+
+        root_dir_path = os.path.join(temp_dir, root_dir)
+
+        file_paths = glob.glob(dicom_directory + '/**/*.dcm', recursive=True)
+        DicomPath = file_paths[0]
+
+
+
+
+
+
+        
+        # with tempfile.NamedTemporaryFile(delete=False, suffix=".dcm") as temp_dicom:
+        #     DicomPath = temp_dicom.name
+        #     temp_dicom.write(await file.read())
 
 
         output_filename = os.path.basename(DicomPath).replace(".dcm", ".jpg")
